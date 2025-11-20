771c07837103b50fe600d42ee4ac486e2b75738e
gmeniconi <104279568+Mengi00@users.noreply.github.com>
2025-11-20 20:45:48 -0300

    Update vite.ts

--- server/vite.ts
+++ server/vite.ts
@@ -1,7 +1,7 @@
 import express, { type Express } from "express";
 import fs from "fs";
 import path from "path";
-import { createServer as createViteServer, createLogger } from "vite";
+
 import { type Server } from "http";
 import viteConfig from "../vite.config";
 import { nanoid } from "nanoid";
@@ -20,6 +20,9 @@ export function log(message: string, source = "express") {
 }
 
 export async function setupVite(app: Express, server: Server) {
+
+  import { createServer as createViteServer, createLogger } from "vite";
+  
   const serverOptions = {
     middlewareMode: true,
     hmr: { server },
